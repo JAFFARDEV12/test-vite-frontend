815cb54b4e20c086e31fcd552c181cace52a5f86
JAFFARDEV12 <jaffar.dev12@gmail.com>
2025-04-09 22:25:17 +0500

    Delete src directory

--- src/components/authImage/lockImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import lockimage from "../../assets/lock.svg"
-
-
-export default function lockImage() {
-  return (
-    <img src={lockimage} alt="image"/>
-  )
-}
--- src/constants/content.js
+++
@@ -1,8 +0,0 @@
-const content = {
-  appName: "DocuMind AI – Empowering Intelligent Document Reasoning",
-  //appTagline: "Unleash the power of AI with Nathasha",
-  appDescription:
-    "DocuMind AI transforms document interaction with smart AI agents, RAG-powered retrieval, and advanced OCR, enabling seamless search, reasoning, and automation for enhanced productivity.",
-};
-
-export default content;
